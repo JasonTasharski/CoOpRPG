8fa40ddbc67fceecfda1bfa5130a1dbb1fb24f6c
J T <Colchis@js-macbook-air.gateway.sonic.net>
2015-06-24 19:17:32 -0700

    One stat can increase

--- scripts.js
+++ scripts.js
@@ -1,12 +1,13 @@
 //Co-op, the text-based RPG.
 var p1 = {
-	'name' : null,
-	'age' : null,
+	'name' : "Hero-kun",
+	'age' : 16,
 	'stats' : {
 		'Martial' : 0,
 		'Physical' : 0,
 		'Social' : 0,
 		'Magic' : 0,
+		'Resilience' : 3
 	},
 	'equipment' : {
 		'thingOne': 0,
@@ -17,13 +18,14 @@ var p1 = {
 	'murders': 0
 };
 var p2 = {
-	'name' : null,
-	'age' : null,
+	'name' : "Sidekick-chan",
+	'age' : 16,
 	'stats' : {
 		'Martial' : 0,
 		'Physical' : 0,
 		'Social' : 0,
 		'Magic' : 0,
+		'Resilience' : 3
 	},
 	'equipment' : {
 		'thingOne': 0,
@@ -55,4 +57,21 @@ function play2() {
 	console.log("Test #2!")
 	document.getElementById("narrationBox").innerHTML = "If you're seeing this, you got the error.";
 	changeNarration.innerHTML = "If you're seeing this, play2() works now and you won.";
+}
+
+// The stuff between this comment and the one above it is for testing.
+
+function martialUp(changingPlayer){
+	if ((changingPlayer.statPoints>0)&&(changingPlayer.stats.Martial<5)){
+		changingPlayer.stats.Martial++; // player is p1 or p1
+		changingPlayer.statPoints--;
+		console.log(changingPlayer.name + " stats:");
+		console.log(changingPlayer.stats);
+		console.log("Points left: " + changingPlayer.statPoints);
+	} else if (changingPlayer.stats.Martial==5){
+		console.log("Martial cap reached. Put your remaining points elsewhere.")
+	} else	{
+		console.log("No points left.");
+		console.log(changingPlayer.name + " stats: " + changingPlayer.stats);
+	}
 }
